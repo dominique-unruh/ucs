36447a6b8b8ef3c8e7aecb6f445afd3cb813242a
David Carlisle <d.p.carlisle@gmail.com>
2022-08-08 20:36:37 +0100

    run perl on unpack

--- build.lua
+++ build.lua
@@ -5,3 +5,15 @@ checkruns=2
 
 installfiles = {"*.def","*.sty"}
 
+
+
+mybundleunpack=bundleunpack
+function bundleunpack(sources, sourcedirs)
+  cp("UnicodeData.txt",string.match(kpse.lookup("UnicodeData.txt"),"(.*[/])"),".")
+  mkdir("data")
+  runcmd("perl -w makeunidef.pl --nocomments --targetdir=data config/*.ucf")
+  rm(".","UnicodeData.txt")
+  mybundleunpack(sources, sourcedirs)
+end
+
+
